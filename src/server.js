bec0253ae2e4c190a9bc82b3d266ce420f54f90e
marshot <124557537+marshot9420@users.noreply.github.com>
2023-02-28 08:54:48 +0900

    Create / Express App

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log("Hello!");
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () => {
+  console.log(`✅ MarsTube server listening on port: ${PORT}🚀`);
+};
+
+app.listen(PORT, handleListening);
