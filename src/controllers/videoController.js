d0fc0ec2eb60db932d9884c6d12cfb0bbf938891
marshot <124557537+marshot9420@users.noreply.github.com>
2023-02-28 12:18:58 +0900

    Feature / search

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", {
     pageTitle: "Home",
     videos,
@@ -62,10 +62,21 @@ export const postEdit = async (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 
-export const search = (req, res) => {
-  return res.render("search", { pageTitle: "Search Video" });
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i"),
+      },
+    });
+  }
+  return res.render("videos/search", { pageTitle: "Search", videos });
 };
 
-export const deleteVideo = (req, res) => {
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
