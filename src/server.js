10f15a810c8b067bb519b32cf26e19397285e770
marshot <124557537+marshot9420@users.noreply.github.com>
2023-02-28 09:01:44 +0900

    Feature / Logger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+
+const logger = morgan("dev");
+
+app.use(logger);
 
 const handleListening = () => {
   console.log(`✅ MarsTube server listening on port: ${PORT}🚀`);
