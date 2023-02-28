de57ad313e23ed7b900b93fd7364517868cccfdf
marshot <124557537+marshot9420@users.noreply.github.com>
2023-02-28 12:06:16 +0900

    Feature / getEdit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -38,8 +38,13 @@ export const watch = async (req, res) => {
   return res.render("videos/watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
-  return res.render("videos/edit", { pageTitle: "Edit Video" });
+export const getEdit = async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("videos/edit", { pageTitle: `Edit ${video.title}`, video });
 };
 
 export const postEdit = (req, res) => {
