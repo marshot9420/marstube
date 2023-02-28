a1dc349a13da452f1a52f84bbb9f91ef6cdbc468
marshot <124557537+marshot9420@users.noreply.github.com>
2023-03-01 08:34:16 +0900

    Feature / Github Sign In

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
+  finishGithubSignin,
   getEdit,
   postEdit,
   profile,
   signout,
+  startGithubSignin,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
@@ -11,5 +13,7 @@ const userRouter = express.Router();
 userRouter.route("/signout").get(signout);
 userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.route("/:id").get(profile);
+userRouter.route("/github/start").get(startGithubSignin);
+userRouter.route("/github/finish").get(finishGithubSignin);
 
 export default userRouter;
