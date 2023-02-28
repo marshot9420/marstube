7ab3f082b557c9921c7c291d6dedea1e1d5ea2c3
marshot <124557537+marshot9420@users.noreply.github.com>
2023-03-01 08:22:42 +0900

    Feature / Mongo Store

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -19,10 +21,15 @@ app.use(
     secret: "914fj10s1902e19!j9d0103",
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/marstube",
+    }),
   })
 );
 
 app.use("/static", express.static("assets"));
+
+app.use(localsMiddleware);
 
 app.use("/", rootRouter);
 app.use("/users", userRouter);
