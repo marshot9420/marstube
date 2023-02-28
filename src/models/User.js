9de7992485e23ccf7f86a6eb6efadb271fd1aff2
marshot <124557537+marshot9420@users.noreply.github.com>
2023-02-28 12:46:42 +0900

    Feature / Hash Password

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -6,6 +7,12 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: String,
+});
+
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
