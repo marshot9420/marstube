03948ebe6bc4b161f3c8e22fba166d00eab11c0f
marshot <124557537+marshot9420@users.noreply.github.com>
2023-02-28 09:04:22 +0900

    Setting / Pug

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ import morgan from "morgan";
 const PORT = 4000;
 
 const app = express();
-
 const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
 
