74bc955b25ecb578ae03ee363a730570061bd686
marshot <124557537+marshot9420@users.noreply.github.com>
2023-03-01 17:39:16 +0900

    Feature / Hidden Menu

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -2,3 +2,11 @@ import "../scss/styles.scss";
 import "../img/logo.png";
 import "../img/marstube.png";
 import "../img/404.jpg";
+
+const headerAvatar = document.querySelector(".header__avatar");
+const hiddenMenu = document.getElementById("hidden-menu");
+
+headerAvatar.addEventListener("click", () => {
+  hiddenMenu.style.display =
+    hiddenMenu.style.display === "none" ? "block" : "none";
+});
