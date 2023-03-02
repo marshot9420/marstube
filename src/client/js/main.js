d7dc590d9b5de481808fe3e5b9568a0ed8e9b338
marshot <124557537+marshot9420@users.noreply.github.com>
2023-03-02 11:10:00 +0900

    Style / Responsive

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -6,6 +6,8 @@ import "../img/404.jpg";
 const headerAvatar = document.querySelector(".header__avatar");
 const hiddenMenu = document.getElementById("hidden-menu");
 
+hiddenMenu.style.display = "none";
+
 const handleHiddenMenu = () => {
   hiddenMenu.style.display =
     hiddenMenu.style.display === "none" ? "block" : "none";
