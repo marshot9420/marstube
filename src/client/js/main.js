3883ef039e1eec6cf467b8e9b00f016f0b2b99f8
marshot <124557537+marshot9420@users.noreply.github.com>
2023-03-02 09:27:14 +0900

    Style / header

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -6,7 +6,9 @@ import "../img/404.jpg";
 const headerAvatar = document.querySelector(".header__avatar");
 const hiddenMenu = document.getElementById("hidden-menu");
 
-headerAvatar.addEventListener("click", () => {
+const handleHiddenMenu = () => {
   hiddenMenu.style.display =
     hiddenMenu.style.display === "none" ? "block" : "none";
-});
+};
+
+headerAvatar.addEventListener("click", handleHiddenMenu);
