3c2cc61bb154735941e4f1c11c732ecb1a4fad6d
marshot9420 <marshot9420@gmail.com>
2023-03-06 09:49:28 +0900

    Version fix

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,8 +1,9 @@
 import multer from "multer";
 import multerS3 from "multer-s3";
-import aws from "aws-sdk";
+import { S3Client } from "@aws-sdk/client-s3";
 
-const s3 = new aws.S3({
+const s3 = new S3Client({
+  region: "ap-northeast-2",
   credentials: {
     accessKeyId: process.env.AWS_ID,
     secretAccessKey: process.env.AWS_SECRET,
